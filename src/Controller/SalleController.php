c54eb3930ed7c44875d43cc94f196b91659ac95e
aline_pencreach <aline.pencreach@gmail.com>
2022-10-03 09:35:47 +0200

    crud salle ok

---
+++ src/Controller/SalleController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Salle;
+use App\Form\SalleType;
+use App\Repository\SalleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/salle')]
+class SalleController extends AbstractController
+{
+    #[Route('/', name: 'app_salle_index', methods: ['GET'])]
+    public function index(SalleRepository $salleRepository): Response
+    {
+        return $this->render('salle/index.html.twig', [
+            'salles' => $salleRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_salle_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, SalleRepository $salleRepository): Response
+    {
+        $salle = new Salle();
+        $form = $this->createForm(SalleType::class, $salle);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $salleRepository->save($salle, true);
+
+            return $this->redirectToRoute('app_salle_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('salle/new.html.twig', [
+            'salle' => $salle,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_salle_show', methods: ['GET'])]
+    public function show(Salle $salle): Response
+    {
+        return $this->render('salle/show.html.twig', [
+            'salle' => $salle,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_salle_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Salle $salle, SalleRepository $salleRepository): Response
+    {
+        $form = $this->createForm(SalleType::class, $salle);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $salleRepository->save($salle, true);
+
+            return $this->redirectToRoute('app_salle_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('salle/edit.html.twig', [
+            'salle' => $salle,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_salle_delete', methods: ['POST'])]
+    public function delete(Request $request, Salle $salle, SalleRepository $salleRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$salle->getId(), $request->request->get('_token'))) {
+            $salleRepository->remove($salle, true);
+        }
+
+        return $this->redirectToRoute('app_salle_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/SalleType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Salle;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SalleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('adresse')
+            ->add('is_active')
+            ->add('structure')
+            ->add('permissions')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Salle::class,
+        ]);
+    }
+}
