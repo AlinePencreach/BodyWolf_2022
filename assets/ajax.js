f5cf7553c324179c444c1488e885bb1a542d782d
aline_pencreach <aline.pencreach@gmail.com>
2022-10-12 14:30:28 +0200

    building ajax request

---
+++ assets/ajax.js
@@ -0,0 +1,57 @@
+// filtre :
+let jsStructure;
+
+// lorsque je clique sur le bouton activés seule les structures activées s'affichent
+let active = document.getElementsById('js-active')
+
+function activeStructure(){
+    if (js-data-structure === isActive) {
+        jsStructure.style.display = 'block'; 
+    }else {
+        jsStructure.style.display = 'none';
+    }
+}
+
+//APPEL DES LISTENER :
+active.addEventListner('click', function(){
+    activeStructure();
+});
+
+
+// lorsque je clique sur le bouton désactivés seule les structures déactivées s'affichent
+let unactive = document.getElementsById('js-unactive')
+
+function unactiveStructure(){
+    if (js-data-structure === isActive) {
+        jsStructure.style.display = 'none'; 
+    }else {
+        jsStructure.style.display = 'block';
+    }
+}
+
+//APPEL DES LISTENER :
+unactive.addEventListner('click', function(){
+    unactiveStructure();
+});
+
+
+
+// lorsque je clique sur le bouton toute, toutes les structures s'affichent
+let all = document.getElementsById('js-all')
+
+function allStructure(){
+    if (js-data-structure === isActive) {
+        jsStructure.style.display = 'block'; 
+    }else {
+        jsStructure.style.display = 'block';
+    }
+}
+
+//APPEL DES LISTENER :
+all.addEventListner('click', function(){
+    allStructure();
+});
+
+
+
+
