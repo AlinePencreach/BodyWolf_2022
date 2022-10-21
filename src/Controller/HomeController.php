b7d6cc96fbcdc58765323167e8dd27b8e0da6046
aline_pencreach <aline.pencreach@gmail.com>
2022-10-21 13:59:59 +0200

    validation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,11 +10,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            
-        ]);
+        return $this->render('home/index.html.twig', []);
     }
 }
